0655d044d9b3c939f15f4d40bd699bb6695742c2
João Cunha <joao.cunha@cldware.com>
2024-02-23 10:40:28 +0000

    refactor

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,6 +2,14 @@
 local wezterm = require 'wezterm';
 local config = {}
 
-config.color_scheme = 'Batman'
+config.color_scheme = 'Nord'
+
+config.keys = {
+  {
+    key = 'r',
+    mods = 'CMD|SHIFT',
+    action = wezterm.action.ReloadConfiguration,
+  },
+}
 
 return config
